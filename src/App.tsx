231c4b00f2b42f7aac821e5e53ccc9219eda3ded
Artur Sapek <me@artur.co>
2024-04-16 20:36:06 -0400

    dicking around

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { Connection, PublicKey } from '@solana/web3.js';
 import './App.css'
+import {getAssociatedTokenAddress} from '@solana/spl-token';
+
+const CHEESE = new PublicKey('AbrMJWfDVRZ2EWCQ1xSCpoVeVgZNpq1U2AoYG98oRXfn');
+const CHEESE_COLOR = '#fcba03';
+
+const WIDTH = 460;
+const HEIGHT = 280;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setConnected] = useState(false);
+  const [tokenBalance, setTokenBalance] = useState<number | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleWalletConnect = async () => {
+
+    const network = 'https://mainnet.helius-rpc.com/?api-key=a769a00e-2047-4cf5-bd06-60535321aadc';
+    const connection = new Connection(network);
+
+    /* @ts-ignore */
+    const walletPublicKey = new PublicKey(window.solana.publicKey.toString());
+
+    const tokenAccount = await getAssociatedTokenAddress(CHEESE, walletPublicKey);
+
+    let { value } = await connection.getTokenAccountBalance(tokenAccount);
+    setTokenBalance(value.amount / 1e6);
+  };
+
+  const connectWallet = () => {
+    if ('solana' in window) {
+      /* @ts-ignore */
+      window.solana.connect().then(() => {
+        handleWalletConnect();
+        setConnected(true);
+      });
+    }
+  };
+
+  useEffect(() => {
+    let canvas = document.getElementById('card');
+    console.log(canvas);
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      const ratio = devicePixelRatio;
+
+      canvas.style.width = `${WIDTH}px`;
+      canvas.width = WIDTH * ratio;
+
+      canvas.style.height = `${HEIGHT}px`;
+      canvas.height = HEIGHT * ratio;
+
+      ctx.scale(devicePixelRatio, devicePixelRatio);
+      
+      ctx.clearRect(0,0,WIDTH,HEIGHT);
+
+      console.log(ctx);
+      ctx.strokeStyle = 'black';
+
+      const MARGIN = 5;
+
+      let roundedCheeseValue = '';
+
+      if (tokenBalance > 1e6) {
+        // Millions
+        roundedCheeseValue = Math.floor(tokenBalance / 1e6) + 'M';
+      } else if (tokenBalance > 1e3) {
+        // Thousands
+        roundedCheeseValue = Math.floor(tokenBalance / 1e3) + 'M';
+      } else {
+        // Singles
+        roundedCheeseValue = tokenBalance;
+      }
+
+      ctx.beginPath();
+      ctx.roundRect(0.5 + MARGIN, 0.5 + MARGIN, WIDTH - MARGIN*2, 30, 10);
+      ctx.rect(0.5 + MARGIN, 10.5, WIDTH - MARGIN*2, 40, 10);
+      ctx.fillStyle = CHEESE_COLOR;
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.roundRect(0.5 + MARGIN, 0.5 + MARGIN, WIDTH - MARGIN*2, HEIGHT - MARGIN*2, 10);
+      ctx.stroke();
+
+      ctx.font = '36px Arial';
+      ctx.fillStyle = 'black';
+      ctx.fillText( roundedCheeseValue + ' $CHEESE', 20.5, HEIGHT - 20.5 );
+
+      ctx.font = 'italic bold 24px Arial';
+      ctx.fillStyle = 'white';
+      ctx.fillText( '$CHEESE CLUB MEMBERSHIP CARD', 14.5, 36.5);
+    }
+  });
+
+  useEffect(() => {
+    if ('solana' in window) {
+      /* @ts-ignore */
+      window.solana.connect({ onlyIfTrusted: true }). then(() => {
+        handleWalletConnect();
+        setConnected(true);
+      });
+    }
+  }, []);
+
+  if (!isConnected) {
+    return (
+      <button onClick={connectWallet} >Connect Wallet</button>
+    );
+  } else if (tokenBalance != null) { 
+    return (
+      <canvas id="card" />
+    )
+  }
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: [
+        'crypto',
+        'http',
+        'https',
+        'stream',
+        'buffer',
+        'url',
+        'os',
+        'zlib',
+      ]
+    })
+  ]
 })
